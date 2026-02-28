cf9c3b1d17d84e0b3489ef03022b50c35ce429f3
sam <sam@smallergames.com>
2026-02-28 10:23:12 -0600

    404 redirect fix

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,31 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+function restoreRouteFrom404Redirect() {
+  const url = new URL(window.location.href);
+  const redirectedRoute = url.searchParams.get("__sg_route");
+  if (!redirectedRoute) {
+    return;
+  }
+
+  let target: URL;
+  try {
+    target = new URL(redirectedRoute, window.location.origin);
+  } catch {
+    return;
+  }
+
+  if (target.origin !== window.location.origin) {
+    return;
+  }
+
+  const normalizedPath = target.pathname.replace(/\/+$/, "") || "/";
+  const restoredPath = `${normalizedPath}${target.search}${target.hash}`;
+  window.history.replaceState(window.history.state, "", restoredPath);
+}
+
+restoreRouteFrom404Redirect();
+
 const rootElement = document.getElementById("root");
 
 if (!rootElement) {
